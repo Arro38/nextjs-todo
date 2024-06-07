828e9900501a63d9b135a8eab597916610eefced
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-06-07 10:47:54 +0400

    update request body

--- app/api/lists/[id]/route.ts
+++ app/api/lists/[id]/route.ts
@@ -2,22 +2,47 @@ import { deleteList, getList, updateList } from "@/prisma/lists";
 import { NextResponse } from "next/server";
 
 export const GET = async ({ params }: { params: { id: string } }) => {
-  const list = await getList(params.id);
-  return NextResponse.json(list, { status: 200 });
+  try {
+    const list = await getList(params.id);
+    return NextResponse.json(list, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 };
 
 export const DELETE = async ({ params }: { params: { id: string } }) => {
-  const list = await deleteList(params.id);
-  return NextResponse.json(list, { status: 200 });
+  try {
+    const list = await deleteList(params.id);
+    return NextResponse.json(list, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 };
 
 export const PUT = async ({
   params,
-  body,
+  request,
 }: {
   params: { id: string };
-  body: { title: string };
+  request: Request;
 }) => {
-  const list = await updateList(params.id, body);
-  return NextResponse.json(list, { status: 200 });
+  try {
+    const body: { title: string } = await request.json();
+    if (!body.title) {
+      return NextResponse.json({ error: "Bad request" }, { status: 400 });
+    }
+    const list = await updateList(params.id, body);
+    return NextResponse.json(list, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 };
--- app/api/lists/route.ts
+++ app/api/lists/route.ts
@@ -2,11 +2,23 @@ import { createList, getLists } from "@/prisma/lists";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const lists = await getLists();
-  return NextResponse.json(lists, { status: 200 });
+  try {
+    const lists = await getLists();
+    return NextResponse.json(lists, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 };
 
-export const POST = async ({ body }: { body: { title: string } }) => {
-  const list = await createList(body);
-  return NextResponse.json(list, { status: 201 });
+export const POST = async (request: Request) => {
+  try {
+    const body: { title: string } = await request.json();
+    const list = await createList(body);
+    return NextResponse.json(list, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Bad request" }, { status: 400 });
+  }
 };
